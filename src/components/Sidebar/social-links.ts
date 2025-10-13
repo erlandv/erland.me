0b5ea872be9cc451d4e254ce951e09b1a9a3df8a
Erland <erlandramdhani@gmail.com>
2025-10-13 16:28:03 +0700

    feat(sidebar): Add mini-flyout to collapsed sidebar. Social icons panel on clik trigger flyout.

---
+++ src/components/Sidebar/social-links.ts
@@ -0,0 +1,31 @@
+export interface SidebarSocialLink {
+  name: string;
+  href: string;
+  label: string;
+}
+
+export const sidebarSocialLinks: SidebarSocialLink[] = [
+  { name: 'x',
+    href: 'https://x.com/erlandzz',
+    label: 'X',
+  },
+  {
+    name: 'instagram',
+    href: 'https://www.instagram.com/erlandramdhani',
+    label: 'Instagram',
+  },
+  {
+    name: 'facebook',
+    href: 'https://www.facebook.com/erlandramdhani',
+    label: 'Facebook',
+  },
+  { name: 'github',
+    href: 'https://github.com/erlandv',
+    label: 'GitHub',
+  },
+  {
+    name: 'bluesky',
+    href: 'https://bsky.app/profile/erland.me',
+    label: 'Bluesky',
+  },
+];
