526229b7a6041663c98c7d61359d08f64a110af1
Erland <erlandramdhani@gmail.com>
2025-10-14 11:11:17 +0700

    fix(format): code format with Prettier code style

--- src/components/Sidebar/social-links.ts
+++ src/components/Sidebar/social-links.ts
@@ -5,10 +5,7 @@ export interface SidebarSocialLink {
 }
 
 export const sidebarSocialLinks: SidebarSocialLink[] = [
-  { name: 'x',
-    href: 'https://x.com/erlandzz',
-    label: 'X',
-  },
+  { name: 'x', href: 'https://x.com/erlandzz', label: 'X' },
   {
     name: 'instagram',
     href: 'https://www.instagram.com/erlandramdhani',
@@ -19,10 +16,7 @@ export const sidebarSocialLinks: SidebarSocialLink[] = [
     href: 'https://www.facebook.com/erlandramdhani',
     label: 'Facebook',
   },
-  { name: 'github',
-    href: 'https://github.com/erlandv',
-    label: 'GitHub',
-  },
+  { name: 'github', href: 'https://github.com/erlandv', label: 'GitHub' },
   {
     name: 'bluesky',
     href: 'https://bsky.app/profile/erland.me',
